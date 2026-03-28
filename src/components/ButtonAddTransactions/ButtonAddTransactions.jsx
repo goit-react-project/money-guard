c07965a16ff368e09a52e2d0b6e86e4abc3afeee
Cigdem Ergal <cigdemtahtasiz@gmail.com>
2026-03-28 14:57:15 +0300

    feat: add add-transaction modal flow

--- src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -1,10 +1,26 @@
-import styles from './ButtonAddTransactions.module.css';
+import { useState } from 'react'
+
+import ModalAddTransaction from '../ModalAddTransaction/ModalAddTransaction'
+import styles from './ButtonAddTransactions.module.css'
 
 const ButtonAddTransactions = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
-    <div>
-      <p></p>
-    </div>
-  );
-};
-export default ButtonAddTransactions;
+    <>
+      <button
+        type="button"
+        className={styles.addTransactionButton}
+        aria-label="Add transaction"
+        onClick={() => setIsModalOpen(true)}
+      >
+        +
+      </button>
+
+      {isModalOpen && (
+        <ModalAddTransaction onClose={() => setIsModalOpen(false)} />
+      )}
+    </>
+  )
+}
+export default ButtonAddTransactions
