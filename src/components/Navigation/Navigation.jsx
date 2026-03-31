89786990f9ef2591605d236e992be7a4b5e7e220
Aslıhan <erdallasll@gmail.com>
2026-03-31 14:55:30 +0300

    fix: add currency NavLink, remove Balance from sidebar, keep sidebar visible on mobile

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import HomeIcon from "../../assets/icons/home-icon.svg?react";
 import StatsIcon from "../../assets/icons/balance-icon.svg?react";
+import CurrencyIcon from "../../assets/icons/currency-icon.svg?react";
 import styles from "./Navigation.module.css";
 
 const Navigation = () => {
@@ -24,6 +25,16 @@ const Navigation = () => {
       >
         <StatsIcon className={styles.icon} />
         <span className={styles.navText}>Statistics</span>
+      </NavLink>
+
+      <NavLink
+        to="/dashboard/currency"
+        className={({ isActive }) =>
+          `${isActive ? styles.activeLink : styles.link} ${styles.mobileCurrencyLink}`
+        }
+      >
+        <CurrencyIcon className={styles.icon} />
+        <span className={styles.navText}>Currency</span>
       </NavLink>
     </nav>
   );
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -2,7 +2,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Navigation from "../../components/Navigation/Navigation";
-import Balance from "../../components/Balance/Balance";
 import styles from "./DashboardPage.module.css";
 
 const DashboardPage = () => {
@@ -14,7 +13,6 @@ const DashboardPage = () => {
       <div className={styles.mainContent}>
         <div className={styles.sidebar}>
           <Navigation />
-          <Balance />
         </div>
         <div className={styles.pageContent}>
           <Outlet />
